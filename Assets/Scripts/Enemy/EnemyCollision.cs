f13d3b46e21d9fe348199b58161ef9d4733a43c6
Tsaplin Sergey <74894518+talatari@users.noreply.github.com>
2023-12-05 22:57:17 +0300

    Add classes EnemySpawner and WallDeath for Enemy.

--- Assets/Scripts/Enemy/EnemyCollision.cs
+++ Assets/Scripts/Enemy/EnemyCollision.cs
@@ -1,21 +1,16 @@
 using System;
 using UnityEngine;
 
-[RequireComponent(typeof(Enemy))]
 public class EnemyCollision : MonoBehaviour
 {
-    private Enemy _enemy;
-
     public event Action<Player> collisionPlayer = delegate { }; 
-    
-    private void Start()
-    {
-        _enemy = GetComponent<Enemy>();
-    }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.TryGetComponent(out Player player))
             collisionPlayer(player);
+
+        if (other.TryGetComponent(out WallDeath wallDeath))
+            Destroy(gameObject);
     }
 }
