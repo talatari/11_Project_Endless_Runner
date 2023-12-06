f1ae1d2988949d214f4c313e1867d62e5cd58ec3
Tsaplin Sergey <74894518+talatari@users.noreply.github.com>
2023-12-06 11:41:00 +0300

    Add TextMeshPro and package UIHealthBar.

---
+++ Assets/Scripts/UIHealthBar/UIHealthBar.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class UIHealthBar : MonoBehaviour
+{
+    [SerializeField] private TMP_Text _tmpText;
+    [SerializeField] private Image _fillBar;
+    
+    private PlayerHealth _playerHealth;
+
+    private void Start()
+    {
+        _playerHealth = FindObjectOfType<PlayerHealth>();
+        _playerHealth.HealthChanged += OnRefreshHealthBar;
+    }
+
+    private void OnDestroy() => 
+        _playerHealth.HealthChanged -= OnRefreshHealthBar;
+
+    private void OnRefreshHealthBar(int currentHealth, int maxHealth)
+    {
+        _tmpText.text = currentHealth + " / " + maxHealth;
+        _fillBar.fillAmount = (float) currentHealth / maxHealth;
+    }
+}
